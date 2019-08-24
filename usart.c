81858f008f62dc776bac89965947c0ed2a8aebcd
yangjc1999 <52656404+yangjc1999@users.noreply.github.com>
2019-08-24 13:08:31 +0800

    c

---
+++ usart.c
@@ -0,0 +1,40 @@
+#include "sys.h"
+#include "usart.h"	  
+////////////////////////////////////////////////////////////////////////////////// 	 
+//����ʹ��ucos,������������ͷ�ļ�����.
+#if SYSTEM_SUPPORT_OS
+#include "includes.h"					//ucos ʹ��	  
+
+void uart_init(u32 bound)
+	{
+  //GPIO�˿�����
+  GPIO_InitTypeDef GPIO_InitStructure;
+	USART_InitTypeDef USART_InitStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;
+	 
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1|RCC_APB2Periph_GPIOA, ENABLE);	//ʹ��USART1��GPIOAʱ��
+  
+	//USART1_TX   GPIOA.9
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9; //PA.9
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;	//������������
+  GPIO_Init(GPIOA, &GPIO_InitStructure);//��ʼ��GPIOA.9
+   
+  //USART1_RX	  GPIOA.10��ʼ��
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10;//PA10
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;//��������
+  GPIO_Init(GPIOA, &GPIO_InitStructure);//��ʼ��GPIOA.10  
+
+  //Usart1 NVIC ����
+  NVIC_InitStructure.NVIC_IRQChannel = USART1_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=3 ;//��ռ���ȼ�3
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;		//�����ȼ�3
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;			//IRQͨ��ʹ��
+	NVIC_Init(&NVIC_InitStructure);	//����ָ���Ĳ�����ʼ��VIC�Ĵ���
+  
+
+  USART_ITConfig(USART1, USART_IT_RXNE, ENABLE);//�������ڽ����ж�
+	#endif
+  USART_Cmd(USART1, ENABLE);                    //ʹ�ܴ���1 
+}
+	
