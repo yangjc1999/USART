6f3aea52583d6331c75b04845aae601f98c53f0b
yangjc1999 <52656404+yangjc1999@users.noreply.github.com>
2019-08-24 13:09:15 +0800

    c

---
+++ main.c
@@ -0,0 +1,29 @@
+#include "led.h"
+#include "delay.h"
+#include "key.h"
+#include "sys.h"
+#include "usart.h"
+
+
+ int main(void)
+ {		
+ 	u16 t;  
+	u16 len;	
+	delay_init();	    	 //��ʱ������ʼ��	  
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); //����NVIC�жϷ���2:2λ��ռ���ȼ���2λ��Ӧ���ȼ�
+	uart_init(115200);	 //���ڳ�ʼ��Ϊ115200
+ 	while(1)
+	{
+		if(USART_RX_STA&0x8000)
+		{					   
+			len=USART_RX_STA&0x3fff;//�õ��˴ν��յ������ݳ���
+			for(t=0;t<len;t++)
+			{
+				USART_SendData(USART1, USART_RX_BUF[t]);//�򴮿�1��������
+				while(USART_GetFlagStatus(USART1,USART_FLAG_TC)!=SET);//�ȴ����ͽ���
+			}
+		
+	}
+ }
+	}
+
